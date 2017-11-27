57469b6a3bbdc0d436a7cfd5175f75c6f047d82a
Melissa Bharratt <melissa.bharratt@gmail.com>
2017-11-27 15:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return ('This one is on me!')
+  } else if (number >= 2001) {
+    return ('I will gladly take your thirty bucks.');
+  }
+}
