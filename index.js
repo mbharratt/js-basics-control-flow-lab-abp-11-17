495bd11063edfdeccb0e189fad5a0113c03bfddd
Melissa Bharratt <melissa.bharratt@gmail.com>
2017-11-27 16:44:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return 'This one is on me!'
+  } else if (number >= 2000 && (number < 2501)){
+    return 'I will gladly take your thirty bucks.'
+  } else if ( number <= 2501){
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tipString){
+  if (tipString === "generous"){
+  return "Thank you so much."
+} else if (tipString === "not as generous"){
+  return "Thank you."
+} else if (tipString === "thanks for everything"){
+  return "Bye."
+  }
+}
